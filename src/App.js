bab1fd02a17f34d8fe83d7dc8881bf9768f06bf4
Tiago <ztiagok@gmail.com>
2022-08-05 15:38:41 -0500

    c1 - Requisito 1 Concluído

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+
+import Login from './components/Login';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Switch>
+        <Route path="/" exact component={ Login } />
+      </Switch>
+      {/* <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
-      </header>
+      </header> */}
     </div>
   );
 }
