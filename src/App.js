6f2a47c2b5a260692b48e529ab27470a55ba57e0
Tiago <ztiagok@gmail.com>
2022-08-05 15:39:52 -0500

    c1 - Requisito 1 Concluído/Correção lint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
 
 import Login from './components/Login';
